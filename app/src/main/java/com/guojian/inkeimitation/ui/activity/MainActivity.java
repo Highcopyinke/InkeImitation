a22c79caa097f11d152fe52db9e99c81664e2b56
Highcopyinke <943924386@qq.com>
2016-10-28 19:09:54 +0800

    修改
    修改

--- app/src/main/java/com/guojian/inkeimitation/ui/activity/MainActivity.java
+++ app/src/main/java/com/guojian/inkeimitation/ui/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package com.guojian.inkeimitation.ui.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.guojian.inkeimitation.R;
 
 
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
